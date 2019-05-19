4afc6c18a9aa694d7edb390fbe753f965566120d
Anderson Oliveira <andervilo@gmail.com>
2019-05-19 17:16:18 -0300

    .

--- src/main/java/com/gigantedepe/IndexController.java
+++ src/main/java/com/gigantedepe/IndexController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-	@GetMapping
+	@GetMapping("")
 	public String index(Model model) {
-		return "index";
+		return "inicio";
 	}
 }
